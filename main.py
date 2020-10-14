a40cd884615e3c848ca9ca099751fcc3fdb0ec2e
fpieschaconr <fpieschaconr@unal.edu.co>
2020-10-13 23:01:10 -0500

    :sparkles: input to page and check result added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 from detector import get
 
@@ -9,5 +10,13 @@ driver.save_screenshot("sudoku.png")
 grid = get("sudoku.png")
 print(grid)
 
+i = 0
+for m in range(9):
+    for n in range(9):
+        square = driver.find_element_by_id("c" + str(i))
+        square.send_keys(str(grid[m, n]))
+        i += 1
+python_button = driver.find_element_by_id("btCheck")
+python_button.click()
 
-driver.close()
+# driver.close()
